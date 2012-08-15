f867b7e38b2c292555b3be5cf0c8cf295092c4c7
peter sokolik <peter.sokolik@smeonline.sk>
2012-08-15 14:37:45 +0200

    Implementation of autoloader/Created root index.php with .htaccess rewrite

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Peter Sokolík <pesoklp13@gmail.com>
+ */
+require_once "WCEG/SplClassLoader.php";
+/**
+ * binding namespace of WCEG Framework to load librarys
+ */
+$loader = new SplClassLoader("WCEG","WCEG");
+$loader->register();
+?>
